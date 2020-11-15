3aaf66f1fb7a62de1b04eafaa3ea899c8bae59c1
Lucas Pickering <lucas@lucaspickering.me>
2020-11-15 09:20:01 -0500

    Switch to rocket

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,34 +1,20 @@
 mod config;
 mod routes;
 
-use crate::{config::LauludConfig, routes::*};
+use crate::config::LauludConfig;
 use mongodb::{options::ClientOptions, Client};
-use serde::Deserialize;
-use tide::Request;
 
-struct UserTrackTags {
-    user_id: String,
-    track_id: String,
-    tags: Vec<String>,
-}
-
-#[derive(Clone)]
-pub struct State {
-    db_client: Client,
-}
-
-#[tokio::main]
-async fn main() -> tide::Result<()> {
+#[rocket::main]
+async fn main() {
     let config = LauludConfig::load().unwrap();
 
     let db_options = ClientOptions::parse(&config.database_url).await.unwrap();
     let db_client = Client::with_options(db_options).unwrap();
 
-    let mut app = tide::with_state(State { db_client });
-
-    // Routes
-    app.at("/api/tracks/:track_id/tags").post(route_create_tag);
-
-    app.listen(config.server_host.as_str()).await?;
-    Ok(())
+    rocket::ignite()
+        .mount("/api", routes::all_routes())
+        .manage(db_client)
+        .launch()
+        .await
+        .unwrap();
 }
--- api/src/routes.rs
+++ api/src/routes.rs
@@ -1,6 +1,7 @@
-use crate::State;
+use mongodb::Client;
+use rocket::{post, routes, Route, State};
+use rocket_contrib::json::Json;
 use serde::{Deserialize, Serialize};
-use tide::{Body, Request};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct Track {
@@ -13,11 +14,36 @@ struct CreateTagBody {
     tags: Vec<String>,
 }
 
-pub async fn route_create_tag(mut req: Request<State>) -> tide::Result<Body> {
-    let CreateTagBody { tags } = req.body_json().await?;
-    let track = Track {
-        track_id: req.param("track_id")?.into(),
-        tags,
-    };
-    Ok(Body::from_json(&track)?)
+struct UserTrackTags {
+    user_id: String,
+    track_id: String,
+    tags: Vec<String>,
+}
+
+/// Function that exports all routes in this file
+pub fn all_routes() -> Vec<Route> {
+    routes![route_get_track, route_create_tag]
+}
+
+#[post("/tracks/<track_id>/tags", format = "json", data = "<body>")]
+async fn route_get_track(
+    track_id: String,
+    body: Json<CreateTagBody>,
+    db_client: State<'_, Client>,
+) -> Json<Track> {
+    let tags = body.to_owned().tags;
+    let track = Track { track_id, tags };
+    Json(track)
+}
+
+#[post("/tracks/<track_id>/tags", format = "json", data = "<body>")]
+async fn route_create_tag(
+    track_id: String,
+    body: Json<CreateTagBody>,
+    db_client: State<'_, Client>,
+) -> Json<Track> {
+    let CreateTagBody { tags } = body.to_owned();
+
+    let track = Track { track_id, tags };
+    Json(track)
 }
