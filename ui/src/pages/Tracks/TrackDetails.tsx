a6427a365b19fff72e5efa5b7070cbecfd7b9b63
Lucas Pickering <lucas@lucaspickering.me>
2020-11-21 17:58:54 -0500

    add tag stuff

--- ui/src/pages/Tracks/TrackDetails.tsx
+++ ui/src/pages/Tracks/TrackDetails.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "react-query";
 import {
+  Button,
   Card,
   CardContent,
   CardHeader,
   Chip,
   makeStyles,
+  TextField,
 } from "@material-ui/core";
+import axios from "axios";
 
 import { TaggedTrack } from "util/schema";
 import AlbumArt from "components/generic/AlbumArt";
@@ -18,6 +22,10 @@ interface Props {
 
 const TrackDetails: React.FC<Props> = ({ track }) => {
   const classes = useStyles();
+  const [newTagText, setNewTagText] = useState<string>();
+  const [createTag, { status }] = useMutation(() =>
+    axios.post(`/api/tracks/${track.track.id}/tags`, { tags: [newTagText] })
+  );
 
   return (
     <Card>
@@ -30,6 +38,20 @@ const TrackDetails: React.FC<Props> = ({ track }) => {
         {track.tags.map((tag) => (
           <Chip key={tag} label={tag} />
         ))}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            createTag();
+          }}
+        >
+          <TextField
+            id="new-tag-input"
+            label="New Tag"
+            value={newTagText}
+            onChange={(e) => setNewTagText(e.target.value)}
+          />
+          <Button type="submit">Add</Button>
+        </form>
       </CardContent>
     </Card>
   );
