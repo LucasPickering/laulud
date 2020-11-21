1398f57c2de56dc8ecfed35fbe5406a787c2bb72
Lucas Pickering <lucas@lucaspickering.me>
2020-11-21 15:59:01 -0500

    search bar

--- ui/src/components/generic/SearchBar.tsx
+++ ui/src/components/generic/SearchBar.tsx
@@ -1,49 +1,40 @@
 import React from "react";
 import { Search as SearchIcon } from "@material-ui/icons";
-import { fade, InputBase, makeStyles } from "@material-ui/core";
+import { InputBase, makeStyles } from "@material-ui/core";
 import clsx from "clsx";
 
-const useStyles = makeStyles(
-  ({ breakpoints, palette, shape, spacing, transitions }) => ({
-    search: {
-      position: "relative",
-      borderRadius: shape.borderRadius,
-      backgroundColor: fade(palette.common.white, 0.15),
-      "&:hover": {
-        backgroundColor: fade(palette.common.white, 0.25),
-      },
-      marginRight: spacing(2),
-      marginLeft: 0,
-      width: "100%",
-      [breakpoints.up("sm")]: {
-        marginLeft: spacing(3),
-        width: "auto",
-      },
+const useStyles = makeStyles(({ palette, shape, spacing }) => ({
+  search: {
+    position: "relative", // Needed because the icon is absolute
+    display: "flex",
+    alignItems: "center",
+    borderRadius: shape.borderRadius,
+    backgroundColor: palette.grey[800],
+    "&:hover": {
+      backgroundColor: palette.grey[700],
     },
-    searchIcon: {
-      padding: spacing(0, 2),
-      height: "100%",
-      position: "absolute",
-      pointerEvents: "none",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    inputRoot: {
-      color: "inherit",
-    },
-    inputInput: {
-      padding: spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${spacing(4)}px)`,
-      transition: transitions.create("width"),
-      width: "100%",
-      [breakpoints.up("md")]: {
-        width: "20ch",
-      },
-    },
-  })
-);
+  },
+  searchIcon: {
+    padding: spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+    width: "100%",
+  },
+  inputInput: {
+    padding: spacing(1),
+    // horizontal padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${spacing(4)})`,
+    width: "100%",
+  },
+}));
+
 interface Props {
   className?: string;
   value?: string;
@@ -56,15 +47,6 @@ const SearchBar: React.FC<Props> = ({ className, value, onChange }) => {
   const classes = useStyles();
 
   return (
-    // <div className={className}>
-    //   <InputBase
-    //     required
-    //     placeholder="Search"
-    //     value={value}
-    //     onChange={onChange}
-    //   />
-    //   <SearchIcon />
-    // </div>
     <div className={clsx(classes.search, className)}>
       <div className={classes.searchIcon}>
         <SearchIcon />
