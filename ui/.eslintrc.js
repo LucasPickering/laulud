d64a2480c24e15973f549862fce3c2b7d07d8261
Lucas Pickering <lucas@lucaspickering.me>
2022-09-21 17:40:11 -0400

    Used skeleton man packages

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -1,58 +1,6 @@
 module.exports = {
-  env: {
-    node: true, // for "process"
-    browser: true,
-    es6: true,
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: "./tsconfig.json",
-    tsconfigRootDir: __dirname,
-  },
-  plugins: [
-    "@typescript-eslint",
-    "react",
-    "react-hooks",
-    "prettier",
-    "jsx-a11y",
-  ],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-    "plugin:jsx-a11y/recommended",
-  ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  settings: {
-    react: {
-      pragma: "React",
-      version: "detect",
-    },
-  },
+  extends: ["@lucaspickering/eslint-config/react"],
   rules: {
-    "no-console": "warn",
-    "no-unused-vars": "off",
-    "react/prop-types": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error",
-    "react/display-name": "off",
-    "jsx-a11y/no-autofocus": "off",
-    "@typescript-eslint/no-unused-vars": "error",
-    "@typescript-eslint/no-explicit-any": ["error", { fixToUnknown: true }],
-    "@typescript-eslint/explicit-function-return-type": [
-      "error",
-      { allowExpressions: true, allowTypedFunctionExpressions: true },
-    ],
-    "@typescript-eslint/no-object-literal-type-assertion": "off",
-    "@typescript-eslint/no-inferrable-types": [
-      "error",
-      { ignoreParameters: true },
-    ],
-    "@typescript-eslint/camelcase": "off",
     "no-restricted-syntax": [
       "error",
       {
@@ -63,23 +11,4 @@ module.exports = {
       },
     ],
   },
-  overrides: [
-    {
-      // Special config files
-      files: ["*.js"],
-      parserOptions: {
-        ecmaVersion: 3,
-      },
-      rules: {
-        "@typescript-eslint/no-var-requires": "off",
-      },
-    },
-    {
-      files: ["*.test.ts"],
-      plugins: ["jest"],
-      env: {
-        jest: true,
-      },
-    },
-  ],
 };
